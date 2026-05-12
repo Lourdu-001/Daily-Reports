8897ac66effc037418806b3206434fb1d765441e
Lourdu-001 <lourdushelton@gmail.com>
2026-05-12 12:09:40 +0530

    site moved to ngrok server

--- resources/js/services/api.js
+++ resources/js/services/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'https://splendor-freebase-tartness.ngrok-free.dev/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        'ngrok-skip-browser-warning': 'true',
     },
 });
 
